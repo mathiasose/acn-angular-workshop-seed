51d116a11c5b986330da9cdcca56259fa4ac2e15
Mathias Ose <mathias.ose@gmail.com>
2013-10-11 15:17:14 +0200

    more stuff

--- app/scripts/controllers/mediaplayer.js
+++ app/scripts/controllers/mediaplayer.js
@@ -2,8 +2,31 @@
 
 angular.module('accAngularWorkshopSeedApp')
     .controller('MediaplayerCtrl', function ($scope, $resource, mediaPlayerService) {
-        $scope.mediaPlayers = mediaPlayerService.readAll();
-        $scope.edit = function(id) {
-            console.log(id);
+        var getLatest = function(){
+            $scope.mediaPlayers = mediaPlayerService.readAll();
         }
+
+        getLatest();
+
+        $scope.edit = function(player) {
+            player.editMode = true;
+        }
+
+        $scope.delete = function(player) {
+            alert("Do you really want to delete " + player.name + "?");
+            mediaPlayerService.delete(player._id);
+            getLatest();
+        }
+
+        $scope.save = function(player) {
+            player.editMode = false;
+            mediaPlayerService.update(player);
+            getLatest();
+        }
+
+        $scope.saveNew = function(player) {
+            mediaPlayerService.create(player);
+            getLatest();
+        }
+
     });
